626bdb9e6c97ebb62a50e4503736eb6294895cbc
Franz Wimmer <info@codefoundry.de>
2015-06-24 13:21:50 +0200

    Service-Interfaces erstellt

---
+++ Cocktailizr/Service/IAdminService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using Cocktailizr.Model.Entities;
+
+namespace Cocktailizr.Service
+{
+    // HINWEIS: Mit dem Befehl "Umbenennen" im Menü "Umgestalten" können Sie den Schnittstellennamen "IAdminService" sowohl im Code als auch in der Konfigurationsdatei ändern.
+    [ServiceContract]
+    public interface IAdminService
+    {
+        [OperationContract]
+        bool AddCocktail(Cocktail cocktail);
+
+        [OperationContract]
+        bool ModifyCocktail(Guid cocktailId, Cocktail cocktail);
+
+        [OperationContract]
+        bool RemoveCocktail(Guid cocktailId);
+
+
+        [OperationContract]
+        bool AddZutat(Zutat zutat);
+
+        [OperationContract]
+        bool ModifyZutat(Guid zutatId, Zutat zutat);
+
+        [OperationContract]
+        bool RemoveZutat(Guid zutatId);
+
+    }
+}
---
+++ Cocktailizr/Service/Impl/CocktailService.svc.cs
@@ -0,0 +1,30 @@
+﻿using Cocktailizr.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Cocktailizr.Service.Impl
+{
+    // HINWEIS: Mit dem Befehl "Umbenennen" im Menü "Umgestalten" können Sie den Klassennamen "CocktailService" sowohl im Code als auch in der SVC- und der Konfigurationsdatei ändern.
+    // HINWEIS: Wählen Sie zum Starten des WCF-Testclients zum Testen dieses Diensts CocktailService.svc oder CocktailService.svc.cs im Projektmappen-Explorer aus, und starten Sie das Debuggen.
+    public class CocktailService : ICocktailService
+    {
+        public Cocktail GetRandomCocktail()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Cocktail> GetCocktailsByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Cocktail> GetCocktailsByIndigrents(IEnumerable<Zutat> zutaten)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
