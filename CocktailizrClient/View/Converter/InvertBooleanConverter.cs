c5ec2e1c36a8b212ba6f6b9abbab1a666644a18f
Michael <m.hutterer@gmx.de>
2015-07-08 11:51:58 +0200

    Converter hinzugefügt.
    ViewModelBase erstellt.

---
+++ CocktailizrClient/View/Converter/InvertBooleanConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace CocktailizrClient.View.Converter
+{
+    public class InvertBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (value is bool) ? !(value as bool?) : false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CocktailizrClient/View/Converter/ObjectInstanciatedToVisibilityConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace CocktailizrClient.View.Converter
+{
+    public class ObjectInstanciatedToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var boolValue = value != null;
+
+            return !boolValue ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
