4dcc7c9a071d2fda9fa8c0aabe1d250a1ab07897
Martin <martin.neumayer@stud.fh-rosenheim.de>
2015-07-09 16:53:41 +0200

    DataGrids hinzugefügt

--- CocktailizrClient/ViewModel/AdminViewModel.cs
+++ CocktailizrClient/ViewModel/AdminViewModel.cs
@@ -1,7 +1,46 @@
-﻿namespace CocktailizrClient.ViewModel
+﻿using CocktailizrClient.AdminServiceReference;
+using CocktailizrClient.CocktailServiceReference;
+using CocktailizrTypes.Model.Entities;
+
+namespace CocktailizrClient.ViewModel
 {
     public class AdminViewModel : CocktailizrClientViewModelBase
     {
+        #region Proprties
+        private Cocktail _recentCocktail;
+
+        public Cocktail RecentCocktail
+        {
+            get { return _recentCocktail; }
+            set
+            {
+                _recentCocktail = value;
+                RaisePropertyChanged(() => RecentCocktail);
+            }
+        }
+
+
+
+        public AdminServiceClient AdminServiceClient { get; set; }
+
+        public CocktailServiceClient CocktailServiceClient { get; set; }
+        #endregion
+
+        #region Commands
+
+        #endregion
+
+        #region Constructor
+        public AdminViewModel(AdminServiceClient adminServiceClient, CocktailServiceClient cocktailServiceClient)
+        {
+            AdminServiceClient = adminServiceClient;
+            CocktailServiceClient = cocktailServiceClient;
+        }
+        #endregion
+
+        #region Methods
+
+        #endregion
 
     }
 }
